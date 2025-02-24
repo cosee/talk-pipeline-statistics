f3f329ba41ccbe2288998027b80439da7b989a5d
Nils Knappmeier <nils.knappmeier@cosee.biz>
2025-02-24 22:40:04 +0100

    settings

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -1,15 +1,24 @@
 function getRequiredEnvVar(name: string): string {
-  const value = Deno.env.get(name);
-  if (!value) {
-    throw new Error(`${name} is not set in the environment variables`);
-  }
-  return value;
+    const value = Deno.env.get(name);
+    if (!value) {
+        throw new Error(`${name} is not set in the environment variables`);
+    }
+    return value;
 }
 
 export function loadSettings() {
-  const gitlabURL = getRequiredEnvVar("GITLAB_URL");
-  const gitlabToken = getRequiredEnvVar("GITLAB_TOKEN");
-  const gitlabProjectId = getRequiredEnvVar("GITLAB_PROJECT_ID");
-  const gitlabProjectPath = getRequiredEnvVar("GITLAB_PROJECT_PATH");
-  return { gitlabURL, gitlabToken, gitlabProjectId, gitlabProjectPath };
+    return {
+        get gitlabURL() {
+            return getRequiredEnvVar("GITLAB_URL");
+        },
+        get gitlabToken() {
+            return getRequiredEnvVar("GITLAB_TOKEN");
+        },
+        get gitlabProjectId() {
+           return  getRequiredEnvVar("GITLAB_PROJECT_ID");
+        },
+        get gitlabProjectPath() {
+            return getRequiredEnvVar("GITLAB_PROJECT_PATH");
+        },
+    };
 }
