7459d7b468f86455ede82dc59520968f7f0a8df8
Nils Knappmeier <nils.knappmeier@cosee.biz>
2025-02-25 10:51:11 +0100

    settings

---
+++ src/iterate-pipelines/iterate-graphql.ts
@@ -0,0 +1,39 @@
+// deno-lint-ignore-file no-explicit-any
+import { loadSettings } from "../common/settings.ts";
+const { gitlabURL, gitlabToken, gitlabProjectPath } = loadSettings();
+
+import {
+    gql,
+    GraphQLClient,
+} from "https://deno.land/x/graphql_request@v4.1.0/src/index.ts";
+
+const jobsQuery = gql`
+    query ProjectJobs($fullProjectPath: ID!, $after: String) {
+        project(fullPath: $fullProjectPath) {
+            jobs(after: $after, first: 10) {
+                count
+                pageInfo {
+                    endCursor
+                    hasNextPage
+                }
+                edges {
+                    node {
+                        id
+                        name
+                        duration
+                        startedAt
+                        status
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const client = new GraphQLClient(`${gitlabURL}/api/graphql`, {
+    headers: {
+        Authorization: `Bearer ${gitlabToken}`,
+    },
+});
+
+
--- src/watch-main-pipeline/show-main-pipeline.ts
+++ src/watch-main-pipeline/show-main-pipeline.ts
@@ -2,15 +2,11 @@ import { loadSettings } from "../common/settings.ts";
 
 const { gitlabToken, gitlabURL } = loadSettings();
 
-// https://docs.gitlab.com/ee/api/pipelines.html#list-project-pipelines
-const response = await fetch(
-  gitlabURL +
-    `/api/v4/projects/865/pipelines?per_page=2&ref=main`,
-  {
-    headers: {
-      "PRIVATE-TOKEN": gitlabToken,
-    },
-  },
-);
 
-console.log(await response.json());
+// https://docs.gitlab.com/ee/api/pipelines.html#list-project-pipelines
+const url = `${gitlabURL}/api/v4/projects/865/pipelines?per_page=2`
+
+// osascript -e 'tell application "System Events" to tell every desktop to set picture to "/Users/knappmeier/tmp/pexels-chevanon-1108116.jpg" as POSIX file'
+// osascript -e 'tell application "System Events" to tell every desktop to set picture to "/Users/knappmeier/Nextcloud/nils/cosee/sonstiges/cosee-background.png" as POSIX file'
+
+
